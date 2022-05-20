38c9fe67a8bde114bf2c50dff46da454f6bf76bb
Quentin <Q.Pavie@etudiant.ecolemelies.fr>
2022-05-20 11:25:25 +0200

    Grow
    add grow

--- Assets/Scripts/SnakeController.cs
+++ Assets/Scripts/SnakeController.cs
@@ -6,11 +6,18 @@ public class SnakeController : MonoBehaviour
 {
     private Vector2Int currentInput;
     private bool isAlive = true;
+    private bool fruitEaten;
+    private int eatenFruits;
     [SerializeField] float timeBeforeMove;
     private Rigidbody2D rb;
+    private Transform tail;
+    public GameObject prefab;
 
     private void Start()
     {
+        fruitEaten = true;
+        GameObject Tail = new GameObject("Tail");
+        tail = Tail.transform;
         rb = GetComponent<Rigidbody2D>();
         StartCoroutine(Move());
     }
@@ -30,7 +37,35 @@ public class SnakeController : MonoBehaviour
         {
             var emptySpace = rb.position;
             rb.MovePosition(emptySpace + currentInput);
+            yield return new WaitForEndOfFrame();
+            foreach (Transform child in tail)
+            {
+                var lastPosition = child.transform.position;
+                child.transform.position = emptySpace;
+                emptySpace = lastPosition;
+            }
+            AddTailPart(emptySpace);
             yield return new WaitForSeconds(timeBeforeMove);
         }
+    }
+
+    public void AddTailPart(Vector2 newPartPosition)
+    {
+        if (fruitEaten == false)
+        {
+            return;
+        }
+        else
+        {
+            var instantiateTail = Instantiate(prefab, newPartPosition, Quaternion.identity);
+            instantiateTail.transform.parent = tail;
+            fruitEaten = false;
+        }
+    }
+
+    public void EatFruit()
+    {
+        fruitEaten = true;
+        eatenFruits++;
     }
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,4 +2,24 @@
 
 public class Spawner : MonoBehaviour
 {
+    public BoxCollider2D playZone;
+    private Vector3Int maxSpawnPos;
+    private Vector3Int minSpawnPos;
+    public GameObject fruitPrefab;
+
+    private void Start()
+    {
+        var tempMaxSpawnPos = playZone.bounds.max;
+        var tempMinSpawnPos = playZone.bounds.min;
+        maxSpawnPos = Vector3Int.FloorToInt(tempMaxSpawnPos);
+        minSpawnPos = Vector3Int.FloorToInt(tempMinSpawnPos);
+        SpawnFruit();
+    }
+
+    public void SpawnFruit()
+    {
+        var x = Random.Range(minSpawnPos.x, maxSpawnPos.x);
+        var y = Random.Range(minSpawnPos.y, maxSpawnPos.y);
+        Instantiate(fruitPrefab, new Vector3Int(x, y, 0), Quaternion.identity);
+    }
 }
